56491b69313e49a2015e5b37c153d47c29fd3a58
Kenny Sekhoela <133218651+Kenni95@users.noreply.github.com>
2025-10-02 23:36:07 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from './config/db.js'
+import foodRouter from './routes/FoodRoute.js'
+import userRouter from './routes/userRoute.js' 
+import dotenv from 'dotenv'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+// Load environment variables
+const result = dotenv.config();
+if (result.error) { console.error("Error loading .env file:", result.error); process.exit(1); }
+
+ 
+//app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+
+//DB Connection
+connectDB();
+
+//API endpoint
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/",(req,res)=>{
+  res.send("API Working")
+})
+
+app.listen(port,()=>{
+  console.log(`Server Started on http://localhost:${port}`)
+})
