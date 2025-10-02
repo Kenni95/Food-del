860f613c2132977167491085bdc6eb20e3bbd5a6
Kenny Sekhoela <133218651+Kenni95@users.noreply.github.com>
2025-10-03 00:40:24 +0200

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://kenny:Sekhoela111@cluster0.vagr5jt.mongodb.net/food-delivery').then(()=>console.log('DB Connected'));
-}
