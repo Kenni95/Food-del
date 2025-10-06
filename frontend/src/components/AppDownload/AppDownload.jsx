baeae37ec8bebad387251690bc6028931b28c973
Kenny Sekhoela <133218651+Kenni95@users.noreply.github.com>
2025-10-06 19:17:29 +0200

    Add files via upload

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+      <p>SundayKos App Launching soon! <br /> For a Better Experience and Delicate Dishes.</p>
+      <div className="app-download-plartforms">
+        <img src={assets.play_store} alt="" />
+        <img src={assets.app_store} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload
